d8a22db0cf481f7c4df4a1a882791a3331f31ab2
Ali Gholami <aligholami7596@gmail.com>
2018-04-19 18:47:36 +0430

    Extract feature maps

--- src/capture.py
+++ src/capture.py
@@ -12,3 +12,9 @@ import face_recognition
 # Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
 
+# Extract the features of two image's
+ali_gholami = face_recognition.load_image_file("ali.jpg")
+obama_face_encoding = face_recognition.face_encodings(ali_gholami)[0]
+
+mohammad_khajavi = face_recognition.load_image_file("mohammad.jpg")
+biden_face_encoding = face_recognition.face_encodings(mohammad_khajavi)[0]
