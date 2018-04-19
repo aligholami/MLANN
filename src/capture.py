05881c89bd1739a1bf482788af9127a5926bd25a
Ali Gholami <aligholami7596@gmail.com>
2018-04-19 18:45:16 +0430

    Getting a reference to webcam

---
+++ src/capture.py
@@ -0,0 +1,14 @@
+# ========================================
+# [] File Name : knn.py
+#
+# [] Creation Date : April 2018
+#
+# [] Created By : Ali Gholami (aligholami7596@gmail.com)
+# ========================================
+
+import cv2
+import face_recognition
+
+# Get a reference to webcam #0 (the default one)
+video_capture = cv2.VideoCapture(0)
+
