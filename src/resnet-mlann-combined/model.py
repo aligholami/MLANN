2c712d2e3d7ca25c750209d701e52305a5b3e3c8
Ali Gholami <aligholami7596@gmail.com>
2018-04-21 18:05:34 +0430

    Minor fixes

--- src/resnet-mlann-combined/model.py
+++
@@ -1,7 +0,0 @@
-# ========================================
-# [] File Name : model.py
-#
-# [] Creation Date : April 2018
-#
-# [] Created By : Ali Gholami (aligholami7596@gmail.com)
-# ========================================
