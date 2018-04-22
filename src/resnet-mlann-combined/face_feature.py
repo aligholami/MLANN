b00b4a07fb211cda83b2730032ed192d8809d4a3
Ali Gholami <aligholami7596@gmail.com>
2018-04-23 00:00:30 +0430

    Update: Loads the model and extract features

--- src/resnet-mlann-combined/face_feature.py
+++ src/resnet-mlann-combined/face_feature.py
@@ -7,46 +7,62 @@ import tensorflow as tf
 from architecture import inception_resnet_v1 as resnet
 import numpy as np
 
-class FaceFeature(object):
-    def __init__(self, face_rec_graph, model_path = 'models/model-20170512-110547.ckpt-250000'):
-        '''
 
-        :param face_rec_sess: FaceRecSession object
-        :param model_path:
-        '''
-        print("Loading model...")
+KEEP_PROB = 0.6
+TRAIN_MODEL = True
+DO_NOT_TRAIN_MODEL = False
+
+class FaceFeature(object):
+
+    def __init__(self, face_rec_graph, model_path = 'models/model-20170512-110547.ckpt-250000'):
+
+        print("\n [INFO] Loading the pretrained model...")
         with face_rec_graph.graph.as_default():
             self.sess = tf.Session()
-            self.x = tf.placeholder('float', [None,160,160,3]); #default input for the NN is 160x160x3
+
+            # Default input shape -> [160, 160, 3]
+            self.x = tf.placeholder('float', [None, 160, 160, 3])
             self.embeddings = tf.nn.l2_normalize(
-                                        resnet.inference(self.x, 0.6, phase_train=False)[0], 1, 1e-10); #some magic numbers that u dont have to care about
+                resnet.inference(self.x, KEEP_PROB, phase_train=DO_NOT_TRAIN_MODEL)[0], 1, 1e-10)
 
-            saver = tf.train.Saver() #saver load pretrain model
+            # Restore the pretrained model
+            saver = tf.train.Saver()
             saver.restore(self.sess, model_path)
-            print("Model loaded")
+            print("\n [INFO] Sucessfully loaded model.")
 
 
-    def get_features(self, input_imgs):
-        images = load_data_list(input_imgs,160)
+    def get_features(self, img_dataset):
+        images = load_data_list(img_dataset, 160)
         return self.sess.run(self.embeddings, feed_dict = {self.x : images})
 
 
 
-#some image preprocess stuff
-def prewhiten(x):
+def whitening_transformation(x):
+    """
+        1. Changes the covariance matrix to identity.
+        2. Changes mean vector to zero.
+    """
     mean = np.mean(x)
     std = np.std(x)
     std_adj = np.maximum(std, 1.0 / np.sqrt(x.size))
     y = np.multiply(np.subtract(x, mean), 1 / std_adj)
+
     return y
 
-def load_data_list(imgList, image_size, do_prewhiten=True):
-    images = np.zeros((len(imgList), image_size, image_size, 3))
+def load_data_list(img_dataset, image_size, pre_whiten = True):
+    """
+        1. Performs a whitening transformation on each image.
+        2. Create a batch of whole dataset.
+    """
+
+    images = np.zeros((len(img_dataset), image_size, image_size, 3))
     i = 0
-    for img in imgList:
+
+    for img in img_dataset:
         if img is not None:
-            if do_prewhiten:
-                img = prewhiten(img)
+            if pre_whiten:
+                img = whitening_transformation(img)
             images[i, :, :, :] = img
             i += 1
+
     return images
