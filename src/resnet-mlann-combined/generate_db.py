ce8ba813b27b1e474757833935e61f42c504d63a
Ali Gholami <aligholami7596@gmail.com>
2018-05-26 21:09:22 +0430

    Update: Frames need to be increased

--- src/resnet-mlann-combined/generate_db.py
+++ src/resnet-mlann-combined/generate_db.py
@@ -7,6 +7,7 @@
 #
 # ========================================
 
+import numpy as np
 from mtcnn_detect import MTCNNDetect
 from align_custom import AlignCustom
 from face_feature import FaceFeature
@@ -15,11 +16,12 @@ import json
 from PIL import Image
 import glob
 import os
+import cv2
 import path
 
 
 RESCALE_FACTOR = 2  # Some scaling configs
-MIN_FACE_SIZE = 80  # Minimum size of 80*80 for each face
+MIN_FACE_SIZE = 150  # Minimum size of 80*80 for each face
 DESIRED_SIZE = 160  # Desired size after the image alignment
 
 
@@ -27,28 +29,36 @@ DESIRED_SIZE = 160  # Desired size after the image alignment
 # Create Tensorflow face recognition graph
 FRGraph = FaceRecGraph()
 
-# Create face detection objects 
-face_detector = MTCNNDetect(FRGraph, scale_factor=RESCALE_FACTOR)
+# Create the aligner instance
+aligner = AlignCustom()
 
 # Create the face feature extractor object
 feature_extractor = FaceFeature(FRGraph)
+
+# Create face detection objects 
+face_detector = MTCNNDetect(FRGraph, scale_factor=RESCALE_FACTOR)
 
 # Open up the faces database(read permission only)
 f = open('./faces_db.txt', 'r')
 
 # Load the database into the ram
-sdata_set = json.loads(f.read())
+data_set = json.loads(f.read())
 
 # Dynamic batch folder selector
 ROOT_DIRECTORY = './lfw/'
 
 for subdir, dirs, files in os.walk(ROOT_DIRECTORY):
-    for file in files:
+    for image in files:
 
         # Convert the image into the proper format
         img_name = os.path.basename(subdir).replace("_", " ")
         
-        while(true):
+        image_path = subdir + "/" + image
+        image = cv2.imread(image_path)
+
+        
+        while True:
+
             rects, landmarks = face_detector.detect_face(image, MIN_FACE_SIZE)
 
             person_imgs_from_different_angles = {"Left" : [], "Right": [], "Center": []}
@@ -60,19 +70,29 @@ for subdir, dirs, files in os.walk(ROOT_DIRECTORY):
                 # Align image and find the position of the face
                 aligned_image, pos = aligner.align(DESIRED_SIZE, image, landmarks[i]);
                 
-                if len(aligned_image) == DESIRED_SIZE and len(aligned_image[0]) == DESIRED_SIZE:
-                
-                # Load the aligned face to the proper angle
-                person_imgs_from_different_angles[pos].append(aligned_frame)
+                if len(aligned_image) == DESIRED_SIZE and len(aligned_image[0]) == DESIRED_SIZE:   
+                    # Load the aligned face to the proper angle
+                    person_imgs_from_different_angles[pos].append(aligned_image)
 
-            # Extract the features from images
-            for pos in person_imgs_from_different_angles:
-                person_features_from_different_angles[pos] = [np.mean(feature_extractor.get_features(person_imgs_from_different_angles[pos]), axis = 0).tolist()]
+
+            print("[INFO] Number of persons in the image: ", rects)
+
             
-            data_set[img_name] = person_features_from_different_angles
+            # Exit on interrupt     
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord("q"):
+                break
 
-            # Write back to db
-            f = open('./faces_db.txt', 'w')
-            f.write(json.dumps(data_set))
+        # Extract the features from images
+        for pos in person_imgs_from_different_angles:
+            person_features_from_different_angles[pos] = [np.mean(feature_extractor.get_features(person_imgs_from_different_angles[pos]), axis = 0).tolist()]
+        
+        data_set[img_name] = person_features_from_different_angles
+
+        # Write back to db
+        f = open('./faces_db.txt', 'w')
+        f.write(json.dumps(data_set))
+
+        print("[INFO] Added image to database... ")
 
 
